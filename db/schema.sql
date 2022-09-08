46f9a3f3cab296c27bc6ec4d64e022cda95b0f61
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-07 21:16:41 -0500

    add database layout in schema add seeds.sql made a db folder and move schema and seeds file into it

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    names VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (departement_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    FOREIGN KEY (roles_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    -- is this correct?
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);

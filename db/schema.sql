dcde4f3b7d7102569c1b11bb9804d4517d052965
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-10 21:17:33 -0500

    test databases is populating server

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,23 @@ USE company_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    names VARCHAR(30) NOT NULL,
+    names VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    FOREIGN KEY (departement_id) REFERENCES department(id) ON DELETE SET NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    roles_id INT,
+    manager_id INT,
     FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (name)
+INSERT INTO department (names)
 VALUES
 ('Software specialists'),
 ('Sales'),
@@ -6,12 +6,12 @@ VALUES
 ('Customer Service');
 
 
-INSERT INTO roles (title, salary, departement_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Software Engineer', 120000, 1),
 ('Front End Developer', 100000, 1),
 ('Inside Sales', 45000, 2),
-('Outside Sales' 60000, 2),
+('Outside Sales', 60000, 2),
 ('Recruitment', 50000, 3),
 ('Labor Relations', 80000, 3),
 ('QA Customer Specialist', 40000, 4),

f5a533f1a4e051660cb35cac8a52393fe9e57d2f
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-11 02:53:43 -0500

    added comment on why the manager id foreign key not working

--- db/schema.sql
+++ db/schema.sql
@@ -22,6 +22,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     roles_id INT,
     manager_id INT,
-    FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL
+    -- Not SOURCE correctly idk why (was working initally and on other computer but not mines)
+    -- FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
